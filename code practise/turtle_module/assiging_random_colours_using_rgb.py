a833b90393c9d2e869edd30cb8582ef56a40d99c
K RUPESH <KurakulaRupesh1234@outlook.com>
2025-07-29 09:30:52 +0530

    Update assiging_random_colours_using_rgb.py

--- code practise/turtle_module/assiging_random_colours_using_rgb.py
+++ code practise/turtle_module/assiging_random_colours_using_rgb.py
@@ -1,19 +1,24 @@
-from turtle import Turtle,Screen
+
+import turtle
 import random
-timmy=Turtle()
+screen=turtle.Screen()
+screen.colormode(255)
+timmy=turtle.Turtle()
 timmy.pensize(10)
 angle=[90,180,270]
+
 def random_colour():
     r=random.randint(0,255)
     g=random.randint(0,255)
     b=random.randint(0,255)
-    randomcolour=(r,g,b)
-    return randomcolour
+   
+    return(r,g,b)
 
 for i in range(0,200):
     timmy.color(random_colour())
     timmy.forward(30)
     timmy.setheading(random.choice(angle))
 
-screen=Screen()
+
 screen.exitonclick()
+
