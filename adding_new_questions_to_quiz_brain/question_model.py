1b9a4d5783efcf3fda0b855b26b0da028255ed9b
K RUPESH <KurakulaRupesh1234@outlook.com>
2025-09-13 16:27:23 +0530

    Delete adding_new_questions_to_quiz_brain directory

--- adding_new_questions_to_quiz_brain/question_model.py
+++
@@ -1,7 +0,0 @@
-class Question:
-    def __init__(self,category,question,correct_answer,incorrect_answers):
-
-        self.category=category
-        self.question=question
-        self.correct_answer=correct_answer
-        self.incorrect_answers=incorrect_answers
